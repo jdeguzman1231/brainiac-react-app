a91c98480141b9660a1a2f3acd90b7dd292e0b64
Joseph DeGuzman <josephdeguzman@Josephs-MBP.fios-router.home>
2021-04-27 14:54:05 -0400

    added multiple choice page

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,8 +6,8 @@ import { createHttpLink } from '@apollo/client'
 import {ApolloProvider} from '@apollo/client'
 
 const httpLink = createHttpLink({
-    uri: 'https://brainiac-game-app.herokuapp.com/graphql'
-    // uri: 'http://localhost:5000/graphql'
+    // uri: 'https://brainiac-game-app.herokuapp.com/graphql'
+    uri: 'http://localhost:5000/graphql'
 })
 
 const client = new ApolloClient({
