5dc1650bf484090a38e334f91740c2efbda6f7a7
Shana Cheng <shanachengg@gmail.com>
2021-05-19 16:13:47 -0400

    Added animated icons on hover

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,9 @@ import { useQuery } from '@apollo/client'
 import { FETCH_PLATFORMS_QUERY } from '../graphql/queries'
 import { isReference } from '@apollo/client';
 import headerimg from '../images/headerimg.svg'
+import glasses from '../images/glasses.svg'
+import handchem from '../images/handchem.svg'
+import flask from '../images/flask.svg'
 import icon from '../images/brainiac-icon.png';
 
 
@@ -28,7 +31,8 @@ function HomePage(props) {
    
     const platforms = allPlatforms.slice(0, 8);
     return (
-        <div style={{ height: '100%', width: '100%', position: 'absolute' }}>
+        <div id="head" style={{ height: '100%', width: '100%', position: 'absolute' }}>
+            
             <div class="header">
                 <div class="content">
                     <div style={{
@@ -38,14 +42,18 @@ function HomePage(props) {
                         lineHeight: '120%',
                         marginLeft: '20%',
                         marginTop: '25%'
-                    }}>
+                    }}> 
                         Build your own learning tools
                         </div>
                     <div style={{ fontSize: '110%', marginLeft: '20%', marginTop: '2%' }}><p>Create and customize simple games for a <br></br>
                         learning experience catered to you</p></div>
                     <Button style={{ marginLeft: '20%', fontSize:'95%', paddingLeft:'2%',  paddingRight:'2%'}} onClick={handleRoute} variant="custom">Get Started</Button>
                 </div>
+                <img class= "floating" style ={{marginLeft:"85%", marginTop:"10%"}} src={glasses}></img>
+                <img class= "floating" style ={{marginLeft:"68%", marginTop:"0%"}} src={flask}></img>
+                <img class= "floating" style ={{marginLeft:"85%", marginTop:"8%"}} src={handchem}></img>
             </div>
+            
 
             <Jumbotron style={{ marginTop: '10px', background: 'radial-gradient(57.56% 81.11% at 85.97% 42.44%, #BEE9FC 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(63.97% 63.97% at 23.26% 36.03%, #F4DCC6 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(46.04% 86.57% at 50% 90.25%, #DDEBCB 0%, rgba(255, 255, 255, 0) 100%), #FDFDF0' }}>
                 <div class="explore">
